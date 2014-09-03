3978a9c661d35e0ab35c9b123ee764b321b0b37c
AlexEvesDeveloper <alexevesdeveloper@gmail.com>
2014-09-03 16:53:44 +0100

    Begun Bootstrapping

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -28,6 +28,7 @@ class AppKernel extends Kernel
             new Gradually\RestBundle\GraduallyRestBundle(),
             new Gradually\JobBundle\GraduallyJobBundle(),
             new Gradually\PurchaseBundle\GraduallyPurchaseBundle(),
+            new Gradually\HomeBundle\GraduallyHomeBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Gradually/HomeBundle/GraduallyHomeBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Gradually\HomeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GraduallyHomeBundle extends Bundle
+{
+}
