f64ebf4f05f9c2b09def91f30c1f7c685ff497f8
AlexEvesDeveloper <alexevesdeveloper@gmail.com>
2014-10-01 07:09:21 -0400

    Clean up, attempting email alters

--- src/Gradually/NotificationBundle/Classes/Notifiers/Email/EmailNewApplicationNotifier.php
+++ src/Gradually/NotificationBundle/Classes/Notifiers/Email/EmailNewApplicationNotifier.php
@@ -2,6 +2,7 @@
 
 namespace Gradually\NotificationBundle\Classes\Notifiers\Email;
 
+use JMS\DiExtraBundle\Annotation as DI;
 use Gradually\NotificationBundle\Classes\Notifiers\NotifierInterface;
 
 class EmailNewApplicationNotifier implements NotifierInterface
@@ -19,6 +20,7 @@ class EmailNewApplicationNotifier implements NotifierInterface
 		return $instance;
 	}
 
+	// pass the mailer object in here?
 	public function notify(array $data)
 	{
 		$application = $data['application'];
@@ -29,10 +31,18 @@ class EmailNewApplicationNotifier implements NotifierInterface
 			$application->getJob()->getTitle(),
 			$application->getCoverNote()
 		);
+
+		$message = \Swift_Message::newInstance()
+			->setSubject(sprintf('Application received: %s', $application->getJob()->getTitle()))
+			->setFrom('noreply@gradually.alexeves.co.uk')
+			->setTo($application->getJob()->getRecruiter()->getEmail())
+			->setBody($content);
+
+		print $application->getJob()->getRecruiter()->getEmail();
+		exit;
 	}
 
-	// insist it remains a singleton
-	private function __construct(){}
+	private function __construct(){
 	private function __clone(){}
 	private function __wakeup(){}
 }
