3af377e0be9ae0d4fdae0d44e75e9957b8eb1aa7
AlexEvesDeveloper <alexevesdeveloper@gmail.com>
2014-10-22 09:53:07 -0400

    CV improvements

--- web/js/jspdf/cv-creator.js
+++ web/js/jspdf/cv-creator.js
@@ -6,7 +6,8 @@ function demoTwoPageDocument() {
 	// keep track of where we are
 	verticalOffset = 10;
 
-//--------------------------------------------------------
+//--TOP BAR------------------------------------------------------
+
 	// name
 	doc.setFontType("bold");
 	doc.text(20, verticalOffset, fullName);
@@ -15,45 +16,48 @@ function demoTwoPageDocument() {
 	doc.setFontSize(10);
 	doc.text(70, verticalOffset, 'E-mail: ' + email);
 
-//--------------------------------------------------------
+//--PROFILE-------------------------------------------------
 
 	verticalOffset += 15;
 	doc.setFontSize(12);
 	doc.setFontType("bold");
 	doc.text(20, verticalOffset, 'Profile');
-	doc.setLineWidth(0.1);
 	verticalOffset += 2;
-	// how far from the left, y axis of the start point, length, y axis of the end point 
+	doc.setLineWidth(0.1);
 	doc.line(20, verticalOffset, 190, verticalOffset);
 
-	// PROFILE
 	verticalOffset += 5;
 	size = 9;
 	doc.setFontType("normal");
 	lines = doc.setFontSize(size).splitTextToSize(profile, 170)
-	console.log(lines.length);
 	doc.text(20, verticalOffset + size / 72, lines);
+
+//--WORK EXPERIENCE-------------------------------------------------
+
 	verticalOffset += (lines.length * 5) + 5;
+	doc.setFontSize(12);
+	doc.setFontType("bold");
+	doc.text(20, verticalOffset, 'Work Experience');
+	verticalOffset += 2;
 	doc.setLineWidth(0.1);
 	doc.line(20, verticalOffset, 190, verticalOffset);
-/*
-	y = 30;
-	console.log(exp);
+
+	verticalOffset += 5;
+	doc.setFontSize(9);	
 	for(var inner in exp){
 		var obj = exp[inner];
-		for(var property in obj){
-			if(obj.hasOwnProperty(property)){
+				
+		doc.setFontType("bold");
+		doc.text(20, verticalOffset, obj["company"]);
+		doc.text(170, verticalOffset, obj["yearFrom"] + " to " + obj["yearTo"]);
+				
+		verticalOffset +=5;
+		doc.setFontType("normal");
+		doc.text(20, verticalOffset, obj["summary"]);
 
-				doc.text(20, y, property + ": " + obj[property]);
-				//console.log(property);
-				//console.log(obj[property]);
-				y += 10;
-			}
-		}
-
-		y += 10;
+		verticalOffset += 10;
 	}
-*/
+
 	
 	// Save the PDF
 	doc.output('save','Test.pdf');
